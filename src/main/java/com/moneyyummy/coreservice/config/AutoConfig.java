ef74e7849e1aeefb07a0feef1a9e519d3ffa16e7
JeonSangYong <tkddyd420@naver.com>
2023-12-16 14:31:29 +0900

    feat: 스프링 빈을 자동 구성하기
    - core-service의 스프링 빈(컴포넌트)들이 각 서비스에서도 자동으로 구성되도록 함

---
+++ src/main/java/com/moneyyummy/coreservice/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.moneyyummy.coreservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.moneyyummy.coreservice")
+public class AutoConfig {
+}
