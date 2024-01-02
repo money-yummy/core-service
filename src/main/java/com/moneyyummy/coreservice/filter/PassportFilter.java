5f165d57c44936a69208dc935c34d565c52c109e
JeonSangYong <tkddyd420@naver.com>
2024-01-02 23:48:18 +0900

    feat: PassportFilter
    - 'moneyyummy.url.skip' 값에 설정된 url 있으면 넘어감
    - 'config.passport-filter' 값이 'true' 이면 스프링 빈으로 등록되어 Filter가 사용됨
    - passport 토큰을 UserModel로 만들어서 request attribute에 넣어주는 필터

---
+++ src/main/java/com/moneyyummy/coreservice/filter/PassportFilter.java
@@ -0,0 +1,48 @@
+package com.moneyyummy.coreservice.filter;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moneyyummy.coreservice.config.PassportFilterCondition;
+import com.moneyyummy.coreservice.model.UserModel;
+import com.moneyyummy.coreservice.util.JwtTokenUtils;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Conditional(PassportFilterCondition.class)
+public class PassportFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper mapper;
+    private final JwtTokenUtils jwtTokenUtils;
+
+    @Value("${moneyyummy.url.skip}")
+    private List<String> skipUrl;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String passport = request.getHeader("passport");
+        Claims claims = jwtTokenUtils.validateAndExtractTokenClaims(passport);
+        String userInfoDto = claims.get("userInfo", String.class);
+        UserModel userModel = mapper.readValue(userInfoDto, UserModel.class);
+        request.setAttribute("userModel", userModel);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return skipUrl.stream()
+                .anyMatch(pattern -> pattern.equals(request.getRequestURI()));
+    }
+}
