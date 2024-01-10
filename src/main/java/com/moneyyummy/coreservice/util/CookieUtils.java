13ab75da8f517e6f4ad66e39ba415554aaad41e0
JeonSangYong <tkddyd420@naver.com>
2024-01-10 16:44:07 +0900

    feat: CookieUtils 쿠키삭제
    - 쿠키 삭제 메서드 추가

--- src/main/java/com/moneyyummy/coreservice/util/CookieUtils.java
+++ src/main/java/com/moneyyummy/coreservice/util/CookieUtils.java
@@ -1,12 +1,16 @@
 package com.moneyyummy.coreservice.util;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CookieUtils {
+
+    public static final String SET_COOKIE = "Set-Cookie";
 
     public ResponseCookie ofToken(String accessToken) {
         return ResponseCookie.from(JwtTokenUtils.AUTHORIZATION, accessToken)
@@ -16,6 +20,21 @@ public class CookieUtils {
     }
 
     public void addCookie(HttpServletResponse response, HttpCookie cookie) {
-        response.setHeader("Set-Cookie", cookie.toString());
+        response.setHeader(SET_COOKIE, cookie.toString());
+    }
+
+    public void deleteCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, "");
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    public void deleteCookie(ServerHttpResponse response, String cookieName) {
+        ResponseCookie cookie = ResponseCookie.from(cookieName, "")
+                .maxAge(0)
+                .path("/")
+                .build();
+        response.addCookie(cookie);
     }
 }
